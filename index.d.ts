cfeb12dd052e677c1bdefee2eee6ebb0135571e9
Ambroise Maupate <ambroise@rezo-zero.com>
2021-02-10 15:08:31 +0100

    Export *

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
 /*
  * Note: `export * from './app'` does not work well with TypeScript < 3.9
  */
-export { HydraCollection, HydraView } from './types/hydra'
+export * from './types/hydra'
 export * from './types/roadiz'
 export * from './types/roadiz-api'
