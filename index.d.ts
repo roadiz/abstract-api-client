60d420319eaf475c81e671c8c832a67e0b7ebdff
Manuel Odelain <manuel@rezo-zero.com>
2021-07-06 15:23:19 +0200

    export global declaration file + remove tests/ from package

--- index.d.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Note: `export * from './app'` does not work well with TypeScript < 3.9
- */
-export * from './types/hydra'
-export * from './types/roadiz'
-export * from './types/roadiz-api'
-export * from './types/common'
